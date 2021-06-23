91a145419808ef4194606201c720fcc6ff902bff
Haofu <haofu.zhu@gmail.com>
2021-06-23 17:16:56 +1000

    Setup the project

---
+++ products/src/bootstrap.js
@@ -0,0 +1,21 @@
+import faker from 'faker';
+
+const mount = (el) => {
+  let products = '';
+
+  for (let index = 0; index < 3; index++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+  
+  el.innerHTML = products;
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#abc-products');
+  if (el) {
+    mount(el)
+  }
+}
+
+export { mount };
